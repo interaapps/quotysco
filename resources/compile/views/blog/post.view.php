2b44dfa2de32683746f56f8102b1dfe4369ab024
JulianFun123 <julianfun123@yahoo.com>
2019-12-07 20:22:24 +0100

    Fixed OG url Tags on posts

--- resources/compile/views/blog/post.view.php
+++ resources/compile/views/blog/post.view.php
@@ -4,7 +4,7 @@
     <meta name="og:title" property="og:title" content="'.htmlspecialchars($postTitle).'">
     <meta name="twitter:creator" property="twitter:creator" content="'.htmlspecialchars($blog["name"]).'">
     <meta name="twitter:site" property="twitter:site" content="'.str_replace("\n","\\n",substr(strip_tags($contents), 0, 100)).'...">
-    <meta property="og:url" content="{{$_SERVER["REQUEST_URI"]}}" />
+    <meta property="og:url" content="'.((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]").'" />
     <meta property="og:title" content="'.htmlspecialchars($postTitle).'" />
     <meta property="og:description" content="'.str_replace("\n","\\n",substr(strip_tags($contents), 0, 50)).'..." />
     '.
