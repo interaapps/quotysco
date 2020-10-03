bc67e582e9dba14641167bb4d95a9e056d2af85b
Julian Gojani <julian@gojani.xyz>
2020-10-03 13:36:01 +0200

    Added API

--- resources/views/landing/explore.php
+++ resources/views/landing/explore.php
@@ -8,7 +8,7 @@ $blogs = [];
 
     <h1 class="center-title">Newest Posts</h1>
 
-    <?php foreach((new databases\PostsTable)->select("*")->order(" id DESC")->limit(10)->get() as $post):?>
+    <?php foreach((new databases\PostsTable)->select("*")->order(" id DESC")->limit(7)->get() as $post):?>
         <?php
         if (!isset($blogs[$post["blogid"]]) ) {
             $blogs[$post["blogid"]] = (new \databases\BlogsTable)->select("*")->where("id", $post["blogid"])->first();
